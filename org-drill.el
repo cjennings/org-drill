cjennings/org-drill

commit 82b50e378db69e96fb3d916785a2f03fddf85637

Show latex overlays in simple card's answer

org-drill.el
@@ -2053,6 +2053,13 @@ RESCHEDULE-FN is the function to reschedule."
            (ignore-errors
              (org-display-inline-images t))
            (org-cycle-hide-drawers 'all)
+           (org-remove-latex-fragment-image-overlays)
+           (save-excursion
+             (org-mark-subtree)
+             (let ((beg (region-beginning))
+                   (end (region-end)))
+               (org--latex-preview-region beg end))
+             (deactivate-mark))
            (org-drill-with-hidden-cloze-hints
             (funcall reschedule-fn session))))))
 

commit d5573d9259ba868c8fbbade1727ccee41ad63650

Bump org-mode version to 9.3
Issue reference: https://gitlab.com/phillord/org-drill/-/issues/19

Caused by https://gitlab.com/phillord/org-drill/-/commit/82b50e378db69e96fb3d916785a2f03fddf85637, which introduced a function created here: https://code.orgmode.org/bzg/org-mode/commit/6d9022df22f86e8b0ea00dfb9179128136edc49a?style=split. Fn is available in org 9.3. Don't know much about packaging for melpa but I figure 82b50e37 should have bumped the org version from 9.2.X at least.

org-drill.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Phillip Lord <phillip.lord@russet.org.uk>
 ;; Author: Paul Sexton <eeeickythump@gmail.com>
 ;; Version: 2.7.0
-;; Package-Requires: ((emacs "25.3") (seq "2.14") (org "9.2.4") (persist "0.3"))
+;; Package-Requires: ((emacs "25.3") (seq "2.14") (org "9.3") (persist "0.3"))
 ;; Keywords: games, outlines, multimedia
 
 ;; URL: https://gitlab.com/phillord/org-drill/issues